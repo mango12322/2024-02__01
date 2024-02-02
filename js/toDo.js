30c1e6b7d31ce871c94201e6033017f1a19eb39e
mango12322 <147010355+mango12322@users.noreply.github.com>
2024-02-02 13:18:38 +0900

    update3

---
+++ js/toDo.js
@@ -0,0 +1,29 @@
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const todoList = document.querySelector("#todo-list");
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintToDo(newToDo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newToDo;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    todoList.appendChild(li);
+}
+
+function handleToDoList(event) {
+    event.preventDefault();
+    const newToDo = toDoInput.value;
+    toDoInput.value = "";
+    paintToDo(newToDo);
+}
+
+toDoForm.addEventListener("submit", handleToDoList);
